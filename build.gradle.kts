09ebe44746e9f83f16618284a207e21962f645ab
jayasuryaenjoy <chintadajayasurya211@gmail.com>
2023-08-07 11:16:51 +0530

    convert json string to map

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation ("com.fasterxml.jackson.core:jackson-core:2.12.5")
+    implementation ("com.fasterxml.jackson.core:jackson-databind:2.12.5")
 }
 
 tasks.test {
